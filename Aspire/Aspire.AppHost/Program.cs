3068638435082a90b654c121b7115624b7ac92ee
MAGS <120091142+MAGS-GH@users.noreply.github.com>
2025-01-02 13:37:04 +0100

    Opgradering til .NET 9.0
    Fjernet alle filer for at gøre plads til .NET 9.0

--- Aspire/Aspire.AppHost/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.API>("api");
-
-builder.AddProject<Projects.BlazorWASM>("BlazorWASM");
-
-builder.Build().Run();
