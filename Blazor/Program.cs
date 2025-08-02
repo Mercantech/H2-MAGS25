ea88a15c7b2aa8ba6b2bdc9f982c9843cc5fc308
MAGS <mathiasgaardsdalsteenberg@gmail.com>
2025-08-02 17:35:59 +0200

    Update API endpoint and add env file to Blazor service
    Changed the default API endpoint in Program.cs to use 'https://h2-mags.mercantec.tech/'. Updated compose.yaml to include an env_file for the Blazor service, allowing environment variables to be loaded from a .env file.

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -19,7 +19,7 @@ public class Program
         // Læs API endpoint fra miljøvariabler eller brug default
         var envApiEndpoint = Environment.GetEnvironmentVariable("API_ENDPOINT");
         Console.WriteLine($"API ENV Endpoint: {envApiEndpoint}");
-        var apiEndpoint = envApiEndpoint ?? "https://h2api.mercantec.tech/";
+        var apiEndpoint = envApiEndpoint ?? "https://h2-mags.mercantec.tech/";
         Console.WriteLine($"API Endpoint: {apiEndpoint}");
 
         // Registrer HttpClient til API service med konfigurerbar endpoint
